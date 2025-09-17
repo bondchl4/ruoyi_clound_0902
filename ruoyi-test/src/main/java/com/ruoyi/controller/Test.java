213e69f0ffc09398d5eba7ef16f8c2fa6fccb9ad
bond <bondchl4@gmail.com>
2025-09-17 10:10:09 +0800

    String s=String.format("你来自%s，我叫%s","中国","charles");

---
+++ ruoyi-test/src/main/java/com/ruoyi/controller/Test.java
@@ -0,0 +1,11 @@
+package com.ruoyi.controller;
+
+public class Test {
+    public static void main(String[] args) {
+        String s=String.format("你来自%s，我叫%s","中国","charles");
+        if(true){
+            System.out.println(s+""+"false");
+        }
+        System.out.println(s);
+    }
+}
