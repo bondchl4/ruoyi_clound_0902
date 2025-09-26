ecfa5bea2c1b3cc3aa03abbe8c118a9b7a361abd
bond <bondchl4@gmail.com>
2025-09-26 23:50:04 +0800

    已经提交到远程git取消提交

--- ruoyi-test/src/main/java/com/ruoyi/controller/Test.java
+++ ruoyi-test/src/main/java/com/ruoyi/controller/Test.java
@@ -19,6 +19,8 @@ class Test {
         String password = "123456";
         String password1 = "123456";
         String password2 = "123456";
+        String password4 = "123456";
+        String password5= "123456";
 
 
         for (int i = 0; i < 2; i++) {  // 只执行一次的循环
