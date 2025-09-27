8405c12c5edd1559bc1f4e47f0de7ebfc2128566
bondchl4 <bondchl4@gmail.com>
2025-09-27 14:25:57 +0800

    Update Test.java

--- ruoyi-test/src/main/java/com/ruoyi/controller/Test.java
+++ ruoyi-test/src/main/java/com/ruoyi/controller/Test.java
@@ -15,6 +15,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 class Test {
     public static void main(String[] args) {
         String name70 = "charles";
+        String name90 = "charles";
+
 
 
 
